3557fd3f3b291e63bf4a4ddb5fbbcd10f651bdcd
Bei Zhang <bzhang@illustrativemathematics.org>
2025-05-03 12:01:50 -0400

    add favicon

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './styles.css'
+import { Metadata } from 'next'
 
-export const metadata = {
-  description: 'A blank template using Payload in a Next.js app.',
-  title: 'Payload Blank Template',
+export const metadata: Metadata = {
+  title: 'Payload Mini Project',
+  description: 'Using Payload blank template',
 }
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
@@ -11,6 +12,9 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
 
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </head>
       <body>
         <main>{children}</main>
       </body>
